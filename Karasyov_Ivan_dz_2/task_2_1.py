4e9a11577e78d3f84babaa0dffefb06eda213fc7
peka97 <vk-vk.karasev@yandex.ru>
2022-04-05 17:04:10 +0300

    1

---
+++ Karasyov_Ivan_dz_2/task_2_1.py
@@ -0,0 +1,10 @@
+# Выяснить тип результата выражений:
+
+result = 15 * 3
+print(type(result))
+result = 15 / 3
+print(type(result))
+result = 15 // 2
+print(type(result))
+result = 15 ** 2
+print(type(result))
