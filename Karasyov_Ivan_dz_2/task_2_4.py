987877e307f161bd0c7fc8b13a41728a308b9471
peka97 <vk-vk.karasev@yandex.ru>
2022-04-07 18:03:05 +0300

    Done 4/5 homework

--- Karasyov_Ivan_dz_2/task_2_4.py
+++ Karasyov_Ivan_dz_2/task_2_4.py
@@ -1,6 +1,5 @@
+
 workers = ['инженер-конструктор Игорь', 'главный бухгалтер МАРИНА', 'токарь высшего разряда нИКОЛАй', 'директор аэлита']
 
-for human in workers:
-    name = human.split()
-    f_name = name[-1].title()
-    print(f"Привет, {f_name}!")
+for human in workers:  # Сразу написал вариант без создания нового списка
+    print(f"Привет, {(human.split()[-1].title())}!")
