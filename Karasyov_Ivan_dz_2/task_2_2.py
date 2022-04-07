0802e8d18af186a90eaea157a92851466a1c59f0
peka97 <vk-vk.karasev@yandex.ru>
2022-04-07 17:46:48 +0300

    Done 2/5 homework

--- Karasyov_Ivan_dz_2/task_2_2.py
+++ Karasyov_Ivan_dz_2/task_2_2.py
@@ -1,8 +1,10 @@
-import string
-
 base_list = ['в', '5', 'часов', '17', 'минут', 'температура', 'воздуха', 'была', '+5', 'градусов']
-for item in base_list:
-    if item.isdigit() or item[0] == "+":
-        item
-words = " ".join(base_list)
-print(words)
+changed_list = []
+for idx, item in enumerate(base_list):
+    if item.isdigit():
+        changed_list.append(f'"{int(item):02d}"')
+    elif item[0] == "+":
+        changed_list.append(f'"+{int(item):02d}"')
+    else:
+        changed_list.append(item)
+print(" ".join(changed_list))
