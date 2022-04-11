e7ea4f5ed735fc59b15e76ed17d74d2bf5f0661c
peka97 <vk-vk.karasev@yandex.ru>
2022-04-11 18:04:21 +0300

    Complete 5/5 tasks

--- Karasyov_Ivan_dz_3/task_3_4.py
+++ Karasyov_Ivan_dz_3/task_3_4.py
@@ -1,34 +1,22 @@
-
-name_dict = {}
-
-
 def thesaurus_adv(names: list):
+    name_dict = {}
     for name in names:
         first_name = name.split()[0]
         second_name = name.split()[1]
         letter_FN = first_name[0]
         letter_SN = second_name[0]
-        print(name)
-        if name_dict.get(letter_SN):
-            if name_dict[letter_SN].values():
-                if name_dict[letter_SN].keys() == letter_SN:
-                    name_dict[letter_SN] = {letter_FN: [*name_dict[letter_SN].values(), name]}
-                elif letter_FN in name_dict[letter_SN].keys():
-                    print("УРА")
+        if name_dict.get(letter_SN) is None:
+            name_dict[letter_SN] = {letter_FN: name}
+        else:
+            if name_dict.get(letter_SN) is not None:
+                if name_dict[letter_SN].get(letter_FN):
                     name_dict[letter_SN].update({letter_FN: [*name_dict[letter_SN].values(), name]})
                 else:
-                    print(0)
-                    print(*name_dict[letter_SN].values())
-                    print(0)
-                    name_dict[letter_SN].update({letter_FN: [*name_dict[letter_SN].values(), name]})
-            else:
-                name_dict[letter_SN] = {letter_FN: [name]}
-        else:
-            print(1)
-            name_dict.update({letter_SN: {letter_FN: name}})
-        print(name_dict)
+                    name_dict[letter_SN].update({letter_FN: [name]})
+    for key in sorted(name_dict.keys()):  # Вариант с сортировкой
+        print(key, ':', name_dict[key])
+    print(f"\n{name_dict}")  # Вариант без сортировки
 
 
 name_full_list = ["Иван Сергеев", "Инна Серова", "Петр Алексеев", "Илья Иванов", "Анна Савельева"]
 thesaurus_adv(name_full_list)
-print(name_dict)
--- Karasyov_Ivan_dz_3/task_3_5.py
+++ Karasyov_Ivan_dz_3/task_3_5.py
@@ -2,29 +2,39 @@ from random import randrange
 
 
 def word_from_joke(joke_list):
+    """Выбирает 3 рандомные шутки"""
+
     return joke_list[randrange(len(joke_list))]
 
 
-def delete_word(word):
-    return None
+def delete_words(lst: list, word: str):
+    """Удаляет уже использованные слова шуток"""
+
+    lst.remove(word)
 
 
-def get_jokes(count: int, flag=0):
+def get_jokes(count: int, flag=0) -> str:
+    """Создает шутку из заданной базы слов. Принимает количество шуток и параметр уникальности"""
+
+    nouns = ["автомобиль", "лес", "огонь", "город", "дом"]
+    adverbs = ["сегодня", "вчера", "завтра", "позавчера", "ночью"]
+    adjectives = ["веселый", "яркий", "зеленый", "утопичный", "мягкий"]
+    words = {}
+    if count > 5:
+        return print("Рекомендуем ввести значение от 1 до 5. Повторите попытку")  # Написал ограничение во избежание ошибок
     if flag == 1:
         for i in range(count):
-            nouns = ["автомобиль", "лес", "огонь", "город", "дом"]
-            adverbs = ["сегодня", "вчера", "завтра", "позавчера", "ночью"]
-            adjectives = ["веселый", "яркий", "зеленый", "утопичный", "мягкий"]
-            joke = [word_from_joke(nouns), word_from_joke(adverbs), word_from_joke(adjectives)]
-            print(joke)
+            words.update({1: word_from_joke(nouns), 2: word_from_joke(adverbs), 3: word_from_joke(adjectives)})
+            delete_words(nouns, words.get(1))
+            delete_words(adverbs, words.get(2))
+            delete_words(adjectives, words.get(3))
+            print(*words.values())
     else:
         for i in range(count):
-            nouns = ["автомобиль", "лес", "огонь", "город", "дом"]
-            adverbs = ["сегодня", "вчера", "завтра", "позавчера", "ночью"]
-            adjectives = ["веселый", "яркий", "зеленый", "утопичный", "мягкий"]
-            joke = [word_from_joke(nouns), word_from_joke(adverbs), word_from_joke(adjectives)]
-            print(joke)
+            words.update({1: word_from_joke(nouns), 2: word_from_joke(adverbs), 3: word_from_joke(adjectives)})
+            print(*words.values())
 
 
 x = int(input("Введите количество шуток: "))
-get_jokes(x)
+y = int(input("Укажите уникальность шуток (0 - откл., 1 - вкл.): "))
+get_jokes(x, y)
