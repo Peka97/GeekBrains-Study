792609b5589082cfe4b274b2f8eb352b47f8060a
Ivan Karasyov <vk-vk.karasev@yandex.ru>
2022-10-11 04:40:17 +0400

    Homework #4

---
+++ Python. Алгоритмы и структуры данных/venv/homework_4/task_4.py
@@ -0,0 +1,64 @@
+"""
+Задание 4.
+Приведены два алгоритма. В них определяется число,
+которое встречается в массиве чаще всего.
+Сделайте профилировку каждого алгоритма через timeit
+Обязательно напишите третью версию (здесь возможно даже решение одной строкой).
+Сделайте замеры и опишите, получилось ли у вас ускорить задачу
+"""
+from timeit import timeit
+from statistics import mode
+
+array = [1, 3, 1, 3, 4, 5, 1]
+
+
+def func_1():
+    m = 0
+    num = 0
+    for i in array:
+        count = array.count(i)
+        if count > m:
+            m = count
+            num = i
+    return f'Чаще всего встречается число {num}, ' \
+           f'оно появилось в массиве {m} раз(а)'
+
+
+def func_2():
+    new_array = []
+    for el in array:
+        count2 = array.count(el)
+        new_array.append(count2)
+
+    max_2 = max(new_array)
+    elem = array[new_array.index(max_2)]
+    return f'Чаще всего встречается число {elem}, ' \
+           f'оно появилось в массиве {max_2} раз(а)'
+
+
+def func_3():
+    # Возможно не самое элегантное решение, но всё же в одну строчку
+    return f'Чаще всего встречается число {[el for el in array if array.count(el) == max([array.count(el) for el in array])][0]}, ' \
+           f'оно появилось в массиве {max([array.count(el) for el in array])} раз(а)'
+
+
+def func_4():
+    # Решение не совсем в одну строчку, однако работает быстрее всех. Если убрать переменную и написать в одну строчку,
+    # то уже работает медленнее остальных.
+    result = max(set(array), key=array.count)
+    return f'Чаще всего встречается число {result}, ' \
+           f'оно появилось в массиве {array.count(result)} раз(а)'
+
+
+if __name__ == '__main__':
+    print(func_1())
+    print(func_2())
+    print(func_3())
+    print(func_4())
+    print(timeit('func_1()', globals=globals(), number=10000))
+    print(timeit('func_2()', globals=globals(), number=10000))
+    print(timeit('func_3()', globals=globals(), number=10000))
+    print(timeit('func_4()', globals=globals(), number=10000))
+    # Функции func_4 удалось превзойти остальные за счёт преобразования в множество и поиск элемента без циклов.
+
+
